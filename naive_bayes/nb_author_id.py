5a247e3ab42d637f5a03e01c2dbbf9ec5738ee81
sergulaydore <sergulaydore@gmail.com>
2015-02-20 01:35:17 -0500

    add timing

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -29,7 +29,9 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 from sklearn.naive_bayes import GaussianNB 
 clf = GaussianNB()
+t0 = time()
 clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
 print (('Accuracy is %.2f') %(clf.score(features_test,labels_test)))
 
 #########################################################
