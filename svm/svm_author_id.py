2771fb212ce9b39abb4da538ce8877c0ca82cf9d
sergulaydore <sergulaydore@gmail.com>
2015-02-25 00:36:04 -0500

    mini project 2 completed

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -15,18 +15,30 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
-
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 #########################################################
 ### your code goes here ###
+clf = svm.SVC(C=10000,kernel='rbf')
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+print accuracy_score(labels_test,pred)
 #########################################################
+
+# MY NOTES
+# SVM is slow for many features
+# Naive Bayes works better for text
 
 
