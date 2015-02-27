3678d61271825025775271972be18fab4de0bd9b
sergulaydore <sergulaydore@gmail.com>
2015-02-27 02:06:58 -0500

    mini project 2

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -14,7 +14,7 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.tree import DecisionTreeClassifier
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -22,12 +22,12 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
-
-
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train,labels_train)
+clf.predict(features_test)
+print ('Accuracy is %.2f') %(clf.score(features_test,labels_test))
 #########################################################
 
 
