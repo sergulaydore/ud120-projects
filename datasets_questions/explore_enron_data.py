398bbf232e1e2e0039d773fd1f2829ba2a1b06d5
sergulaydore <sergulaydore@gmail.com>
2015-03-02 23:31:40 -0500

    ENRON data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,5 +18,21 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+for key in enron_data.keys():
+    print len(enron_data[key])
 
 
+pois = [enron_data[key]['poi'] for key in enron_data.keys()]
+print sum(pois)
+
+salary = [enron_data[key]['salary'] for key in enron_data.keys()
+ if enron_data[key]['salary']!='NaN']
+     
+emails = [enron_data[key]['email_address'] for key in enron_data.keys()
+ if enron_data[key]['email_address']!='NaN']
+
+
+text_data = open('../final_project/poi_names.txt','r')
+names_data = text_data.readlines()
+
+pois_names = [enron_data[name]['poi'] for name in wtf if name in enron_data.keys()]    
