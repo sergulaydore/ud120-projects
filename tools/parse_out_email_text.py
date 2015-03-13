3ff0f6c584978cdac86f78d7e9568cc8b9327bba
sergulaydore <sergulaydore@gmail.com>
2015-03-13 03:08:32 -0400

    stemmed text

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,12 +28,17 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+ #       words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        splitted = text_string.rsplit()
+
+        stemmer = SnowballStemmer("english")
+        for my_word in splitted:
+             ( stemmer.stem(my_word) )
+             words = words + ' ' + ( stemmer.stem(my_word) )       
 
 
 
