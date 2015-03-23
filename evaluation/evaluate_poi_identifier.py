69370cafe68b610e7f103a6c31e3a92648dd7d08
sergulaydore <sergulaydore@gmail.com>
2015-03-23 15:05:54 -0400

    Evaluate POI identifier

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,5 +28,32 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+### it's all yours from here forward!  
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features,labels)
+clf.predict(features)
+print ('Accuracy is %.2f') %(clf.score(features,labels))
+
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(
+     features, labels, test_size=0.30, random_state=42)
+clf = DecisionTreeClassifier()
+clf.fit(features_train,labels_train)
+predict_test = clf.predict(features_test)
+print ('Accuracy of test data is %.2f') %(clf.score(features_test,labels_test))
+from sklearn.metrics import accuracy_score
+import numpy as np
+print ('Accuracy of test data if everything is predicted 0: %.2f') %(accuracy_score(labels_test, 
+                                            np.zeros((len(predict_test)))))
+
+from sklearn.metrics import precision_score                                            
+precision_score(labels_test, predict_test, average=None) 
+from sklearn.metrics import recall_score                                            
+recall_score(labels_test, predict_test, average=None) 
 
 
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+precision_score(true_labels,predictions)
+recall_score(true_labels,predictions)
